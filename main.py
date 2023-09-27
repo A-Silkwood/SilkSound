474ef605ed1558d676ee7bea2fd0d108ea33cf49
Andrew Silkwood <silkwood.andrew@gmail.com>
2023-09-26 18:10:48 -0700

    remove message

--- main.py
+++ main.py
@@ -37,15 +37,6 @@ def main():
     async def on_ready():
         print(f"We have logged in as {client.user}")
 
-    # runs when message is sent to server
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
-
-        if message.content.startswith("$hello"):
-            await message.channel.send("Hello!")
-
     # run bot
     client.run(config.get("BOT_TOKEN"), log_handler=None)
 
