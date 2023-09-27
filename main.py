82a04ebdc8780ec743b9bb4950bec4e80afd0289
Andrew Silkwood <silkwood.andrew@gmail.com>
2023-09-26 17:44:06 -0700

    setup logger

--- main.py
+++ main.py
@@ -9,11 +9,24 @@ def main():
     config = dotenv_values(".env")
 
     # initalize logger
+    logger = logging.getLogger("discord").setLevel(
+        logging.DEBUG if config.get("ENVIRONMENT") == "dev" else logging.INFO
+    )
+    # initalize logger handler
     if not os.path.exists("logs"):
         os.mkdir("logs")
-    handler = logging.FileHandler(
-        filename=os.path.join(os.getcwd(), "logs", "discord.log"), encoding="utf-8", mode="w"
+    handler = logging.handlers.RotatingFileHandler(
+        filename=os.path.join(os.getcwd(), "logs", "discord.log"),
+        encoding="utf-8",
+        maxBytes=32 * 1024 * 1024,  # 32 MiB
+        backupCount=5,  # Rotate through 5 files
     )
+    dt_fmt = "%Y-%m-%d %H:%M:%S"
+    formatter = logging.Formatter(
+        "[{asctime}] [{levelname:<8}] {name}: {message}", dt_fmt, style="{"
+    )
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
 
     # initialize bot
     intents = discord.Intents.default()
@@ -35,7 +48,7 @@ def main():
             await message.channel.send("Hello!")
 
     # run bot
-    client.run(config.get("BOT_TOKEN"))
+    client.run(config.get("BOT_TOKEN"), log_handler=handler)
 
 
 if __name__ == "__main__":
